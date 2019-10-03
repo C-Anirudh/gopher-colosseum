0dfb2d1ea1012ed0000d8205436ca69b2db6ba11
C-Anirudh <canirudh16@gmail.com>
2019-10-03 22:29:43 +0530

    add solution-1 to hello-world

---
+++ hello-world/solution-1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintln(w, `<h1>Hello, World</h1>`)
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.ListenAndServe(":8000", nil)
+}
