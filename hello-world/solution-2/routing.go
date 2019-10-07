d81f02b27e7233246a7d5d1f7a729172f7a16ec2
chanduch1999 <chanduch1999@gmail.com>
2019-10-07 15:42:53 +0530

    update solution-2

--- hello-world/solution-2/routing.go
+++ hello-world/solution-2/routing.go
@@ -9,6 +9,7 @@ func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/about", about)
 	http.HandleFunc("/contact", contact)
+	http.HandleFunc("/profile",profile)
 	http.ListenAndServe(":3000", nil)
 }
 
@@ -31,6 +32,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 <button onclick="window.location.href = '/';">Home</button>
 <button onclick="window.location.href = '/about';">About Me</button>
 <button onclick="window.location.href = '/contact';">Contact</button>
+<button onclick="window.location.href = '/profile';">Profile</button>
 </body>
 </html>`)
 }
@@ -71,4 +73,70 @@ func contact(w http.ResponseWriter, r *http.Request) {
 <button onclick="window.location.href = '/';">Home</button>
 </body>
 </html>`)
+}
+
+
+func profile(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, `<!DOCTYPE html>
+	<html>
+	<head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+	.card {
+	  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+	  max-width: 300px;
+	  margin: auto;
+	  text-align: center;
+	  font-family: arial;
+	}
+	
+	.title {
+	  color: grey;
+	  font-size: 18px;
+	}
+	
+	button {
+	  border: none;
+	  outline: 0;
+	  display: inline-block;
+	  padding: 8px;
+	  color: white;
+	  background-color: #000;
+	  text-align: center;
+	  cursor: pointer;
+	  width: 100%;
+	  font-size: 18px;
+	}
+	
+	a {
+	  text-decoration: none;
+	  font-size: 22px;
+	  color: black;
+	}
+	
+	button:hover, a:hover {
+	  opacity: 0.7;
+	}
+	</style>
+	</head>
+	<body>
+	
+	<h2 style="text-align:center">Profile Card</h2>
+	
+	<div class="card">
+	  <img src="https://timesofindia.indiatimes.com/thumb/msid-67151704,width-400,resizemode-4/67151704.jpg?imglength=215618" alt="Chandu" style="width:100%">
+	  <h1>Chandu</h1>
+	  <p class="title">CEO & Founder, Example</p>
+	  <p>Amrita university University</p>
+	  <div style="margin: 24px 0;">
+		<a href="https://twitter.com/ch_chandu01"><i class="fa fa-twitter"></i></a>  
+		<a href="https://www.linkedin.com/in/chandu-ch-0a9b5218b/"><i class="fa fa-linkedin"></i></a>  
+		<a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a> 
+	  </div>
+	  <p><button>Contact</button></p>
+	</div>
+	<button onclick="window.location.href = '/';">Home</button>
+
+	</body>
+	</html>`)
 }
